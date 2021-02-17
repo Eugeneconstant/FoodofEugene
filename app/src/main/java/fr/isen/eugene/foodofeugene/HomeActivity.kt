3cee0cdd219e5404d9ff84e79f68068e64df01a9
Eugeneconstant <eugene.constant@isen.yncrea.fr>
2021-02-17 09:36:27 +0100

    Intent new version

--- app/src/main/java/fr/isen/eugene/foodofeugene/HomeActivity.kt
+++ app/src/main/java/fr/isen/eugene/foodofeugene/HomeActivity.kt
@@ -1,9 +1,8 @@
 package fr.isen.eugene.foodofeugene
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import fr.isen.eugene.foodofeugene.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -13,18 +12,30 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val monIntent: Intent = Intent(this, category_activity::class.java)
+
         binding.btn1.setOnClickListener {
-            startActivity(monIntent)
+            SCA(Type.ENTREES)
         }
 
         binding.btn2.setOnClickListener {
-            //Toast.makeText(this, "Hello Toast", Toast.LENGTH_LONG).show()
+            SCA(Type.PLATS)
         }
 
         binding.btn3.setOnClickListener {
-            //Toast.makeText(this, "Hello Toast", Toast.LENGTH_LONG).show()
+            SCA(Type.DESSERTS)
         }
 
     }
+
+    private fun SCA(type: Type){
+        val intent = Intent(this, category_activity::class.java)
+        intent.putExtra(CATEGORY_NAME, type)
+        startActivity(intent)
+    }
+    companion object {
+        const val CATEGORY_NAME = "CATEGORY_NAME"
+    }
 }
+
+
+
