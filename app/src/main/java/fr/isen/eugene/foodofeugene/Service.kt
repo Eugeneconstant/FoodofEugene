75aff8ee5a4606eb61c8515521f6e262b2f98cce
Eugeneconstant <eugene.constant@isen.yncrea.fr>
2021-05-03 14:03:20 +0200

    APK version 1

---
+++ app/src/main/java/fr/isen/eugene/foodofeugene/Service.kt
@@ -0,0 +1,8 @@
+package fr.isen.eugene.foodofeugene
+
+import android.bluetooth.BluetoothClass
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
+
+class Service(){
+
+}
---
+++ app/src/main/java/fr/isen/eugene/foodofeugene/ble/detail_ble/DetailDeviceAdapter.kt
@@ -0,0 +1,7 @@
+package fr.isen.eugene.foodofeugene.ble.detail_ble
+
+import android.bluetooth.BluetoothGattService
+
+class DetailDeviceAdapter(private val service: List<BluetoothGattService>) {
+
+}
